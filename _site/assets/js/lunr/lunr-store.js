655301c944f7f8fd61a6f5583851db0d381de12f
Rajith P <rajithpra2@gmail.com>
2021-06-06 10:47:24 +0530

    First Commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "ബങ്കനപ്പള്ളി",
+        "excerpt":"വലുപ്പമേറിയ ഫലമാണ് ലഭിക്കുന്നത്. തൊലി നേർത്തതും, മിനുമിനുപ്പുമുള്ള മാമ്പഴത്തിന് സ്വർണ്ണനിറമുള്ളതുമാണ്. സീസണിന്റെ ആദ്യം തന്നെ കായ്ക്കുന്നതിനാൽ നല്ല വിലകിട്ടുന്നതുമാണ്. സാമാന്യം നല്ല വിളവുതരുന്ന ഫലത്തിന് നല്ലഗന്ധം കൂടിയുണ്ട്. സമാന്യം വലുപ്പമുള്ള ഇതിന് ഭാരം ഏകദേശം 300-600 ( ചിലപ്പോൾ 800 ഗ്രാമിന് മുകളിൽ തൂക്കം വെക്കുന്നത് കാണാറുണ്ട് ).ഗ്രാമും പഞ്ചസാര അളവ് കൂടുതലുമാണ്. Banganapalli sample gallery . ബംഗൻപള്ളി കുടുതലായി കണ്ടുവരുന്നത് ആന്ധ്രാപ്രദേശിലാണ്.അവ നല്ല സുഗന്ധം ഉള്ളതും ഇളം...","categories": [],
+        "tags": [],
+        "url": "/portfolio/Benganapilli/",
+        "teaser": "/assets/images/Banganapalli-700x700.jpg"
+      },{
+        "title": "ബങ്കനപ്പള്ളി",
+        "excerpt":"വലുപ്പമേറിയ ഫലമാണ് ലഭിക്കുന്നത്. തൊലി നേർത്തതും, മിനുമിനുപ്പുമുള്ള മാമ്പഴത്തിന് സ്വർണ്ണനിറമുള്ളതുമാണ്. സീസണിന്റെ ആദ്യം തന്നെ കായ്ക്കുന്നതിനാൽ നല്ല വിലകിട്ടുന്നതുമാണ്. സാമാന്യം നല്ല വിളവുതരുന്ന ഫലത്തിന് നല്ലഗന്ധം കൂടിയുണ്ട്. സമാന്യം വലുപ്പമുള്ള ഇതിന് ഭാരം ഏകദേശം 300-600 ( ചിലപ്പോൾ 800 ഗ്രാമിന് മുകളിൽ തൂക്കം വെക്കുന്നത് കാണാറുണ്ട് ).ഗ്രാമും പഞ്ചസാര അളവ് കൂടുതലുമാണ്. Banganapalli sample gallery . ബംഗൻപള്ളി കുടുതലായി കണ്ടുവരുന്നത് ആന്ധ്രാപ്രദേശിലാണ്.അവ നല്ല സുഗന്ധം ഉള്ളതും ഇളം...","categories": [],
+        "tags": [],
+        "url": "/portfolio/Benganapilli1/",
+        "teaser": "/assets/images/Banganapalli-700x700.jpg"
+      },{
+        "title": "നീലം",
+        "excerpt":"തമിഴ്നാട്ടിൽ ജന്മംകൊണ്ട ഈ ഇനം തെക്കേ ഇന്ത്യയിലെ സുപ്രധാന വാ‍ണിജ്യമൂല്യമുള്ള ഫലങ്ങളിലൊന്നാണ്. തമിഴ്നാട്ടിൽ ചിലേടങ്ങളിൽ കാജാലഡുവെന്നറിയപ്പെടുന്നു. ഈർപ്പമുള്ള കാലവസ്ഥയിൽ നന്നായി വളരുന്ന ഈ ഇനം സൂക്ഷിപ്പ് ഗുണം കൂടുതലയാതിനാൽ കയറ്റുമതിക്ക് കൂടുതൽ അനുയോജ്യമാണ്. മാർച്ച്-ഏപ്രിൽ വരേയാണ് മാമ്പഴക്കാലം. മധുരമേറിയ പഴത്തിന്റെ കഴമ്പ് ഉറപ്പുള്ളതും നാരില്ലാത്തതുമാണ്. മിതവലുപ്പമുള്ള ഇതിന്റെ ഭാരം 200-250 ഗ്രാം കാണാറുണ്ട്. വൈകി മാത്രമേ മാങ്ങ മൂപ്പെത്തൂവെന്നത് ഇതിന്റെ എടുത്തുപറയാവുന്ന ന്യൂനതയാണ്. Neelam sample gallery . മാങ്ങയിൽ...","categories": [],
+        "tags": [],
+        "url": "/portfolio/Neelam/",
+        "teaser": "/assets/images/Neelam-04.png"
+      },{
+        "title": "നീലം",
+        "excerpt":"തമിഴ്നാട്ടിൽ ജന്മംകൊണ്ട ഈ ഇനം തെക്കേ ഇന്ത്യയിലെ സുപ്രധാന വാ‍ണിജ്യമൂല്യമുള്ള ഫലങ്ങളിലൊന്നാണ്. തമിഴ്നാട്ടിൽ ചിലേടങ്ങളിൽ കാജാലഡുവെന്നറിയപ്പെടുന്നു. ഈർപ്പമുള്ള കാലവസ്ഥയിൽ നന്നായി വളരുന്ന ഈ ഇനം സൂക്ഷിപ്പ് ഗുണം കൂടുതലയാതിനാൽ കയറ്റുമതിക്ക് കൂടുതൽ അനുയോജ്യമാണ്. മാർച്ച്-ഏപ്രിൽ വരേയാണ് മാമ്പഴക്കാലം. മധുരമേറിയ പഴത്തിന്റെ കഴമ്പ് ഉറപ്പുള്ളതും നാരില്ലാത്തതുമാണ്. മിതവലുപ്പമുള്ള ഇതിന്റെ ഭാരം 200-250 ഗ്രാം കാണാറുണ്ട്. വൈകി മാത്രമേ മാങ്ങ മൂപ്പെത്തൂവെന്നത് ഇതിന്റെ എടുത്തുപറയാവുന്ന ന്യൂനതയാണ്. Neelam sample gallery . മാങ്ങയിൽ...","categories": [],
+        "tags": [],
+        "url": "/portfolio/Neelam1/",
+        "teaser": "/assets/images/Neelam-04.png"
+      },{
+        "title": "Chocolate Chip Cookies",
+        "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
+        "tags": [],
+        "url": "/recipes/chocolate-chip-cookies/",
+        "teaser": null
+      },{
+        "title": "Oatmeal Cookies",
+        "excerpt":"Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts. Ingredients 1 cup butter, softened 1 cup white sugar 1 cup packed brown sugar 2 eggs 1 teaspoon vanilla extract 2 cups all-purpose flour 1...","categories": [],
+        "tags": [],
+        "url": "/recipes/oatmeal-cookies/",
+        "teaser": null
+      },{
+        "title": "Peanut Butter Cookies",
+        "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
+        "tags": [],
+        "url": "/recipes/peanut-butter-cookies/",
+        "teaser": null
+      }]
